550b9ed7b995262d1448170c0523abb9fd5408c6
Wario19 <konstantin.zerlauth@gmx.at>
2022-10-13 10:24:47 +0200

    Javascript Aufgabe 1 und 2

---
+++ Javascript/01 Variablen/script.js
@@ -0,0 +1,38 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+//Variablen
+
+let a = 7;
+let b = 8;
+let c = 10;
+
+let result = a+b+c;
+
+console.log("Das Ergebnis ist " + result)
+
+console.log("a hat den wert "+ a);
+a=10;
+console.log("a hat den wert "+ a);
+
+console.log("a+b = " + c);
+
+//Aufgaben
+result = a + b + c
+console.log("Das Ergebnis ist " + result)
+result = (a + b) / c
+console.log("Das Ergebnis ist " + result)
+result = c -a + b
+console.log("Das Ergebnis ist " + result)
+result = a + b + c * 3
+console.log("Das Ergebnis ist " + result)
+result = (a + b + c)/4
+console.log("Das Ergebnis ist " + result)
+
+a = 3
+b = a + 3
+c = c -3
+
+console.log("a hat den wert "+ a);
+console.log("b hat den wert "+ b);
+console.log("c hat den wert "+ c);
---
+++ Javascript/02 Strings/script.js
@@ -0,0 +1,10 @@
+let a = 17.4;
+let b = 3.9;
+let result = a/b;
+
+console.log("The result of " + a +  " divided by " + b + " is " + result);
+
+let firstname = "Konstantin";
+let lastname = "Zerlauth";
+
+console.log("Ich heiße " + firstname + " " + lastname);
