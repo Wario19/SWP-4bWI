37f5e3d298246a3c38c166dcfc6d0e693c010be3
Wario19 <konstantin.zerlauth@gmx.at>
2022-10-14 15:30:09 +0200

    Aufgabe 5

---
+++ Javascript/05 IF-Bedingung/script.js
@@ -0,0 +1,37 @@
+let number = 69;
+
+//Aufgabe 1
+
+if(number < 20)
+{
+    console.log("Mini");
+};
+
+if(20 < number && number < 50)
+{
+    console.log("Medium");
+};
+
+if(number > 50)
+{
+    console.log("Large");
+};
+
+console.log(" ");
+
+//Aufgabe 2
+
+if(number <= 7)
+{
+    console.log("Mini");
+};
+
+if(7 < number && number <= 90)
+{
+    console.log("Medium");
+};
+
+if(number > 90)
+{
+    console.log("Large");
+};
