b903c51f28a60a1389825580029fe4597f7a5824
Wario19 <konstantin.zerlauth@gmx.at>
2022-10-14 15:19:46 +0200

    Aufgabe 4

---
+++ Javascript/04 Schleifen/script.js
@@ -0,0 +1,50 @@
+for(let count = 7;count <= 45;count++)
+{
+    console.log(count);
+};
+
+console.log(" ");
+
+for(let count = 9;count <= 99;count++)
+{
+    console.log(count);
+};
+
+console.log(" ");
+
+for(let count = 10;count >= 0;count--)
+{
+    console.log(count);
+};
+
+console.log(" ");
+
+for(let count = 1234;count >= 1207;count--)
+{
+    console.log(count);
+};
+
+console.log(" ");
+
+for(let count = 0;count < 8;count++)
+{
+    console.log("Konstantin Zerlauth");
+};
+
+console.log(" ");
+
+for(let count = 0;count <= 10;count++)
+{
+    console.log(count);
+    if(count == 10)
+    {
+        console.log("Konstantin Zerlauth");
+        for(let count = 10;count >= 0;count--)
+        {
+            console.log(count);
+        };
+    }
+    
+};
+
+console.log(" ");
