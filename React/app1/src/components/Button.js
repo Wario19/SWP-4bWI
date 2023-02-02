07556ed6b601266f9332f4da9cf2a38d284e770c
Wario19 <konstantin.zerlauth@gmx.at>
2023-02-02 10:01:28 +0100

    React app1

---
+++ React/app1/src/components/Button.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import styles from './Button.module.css'
+
+
+export default function Button(props) {
+    const [isOn, setIsOn] = useState(); 
+  return (
+    <div 
+        className={isOn?styles.isOn:styles.isOff} 
+        onClick={()=>{
+        
+        setIsOn(!isOn);
+    }}
+    > 
+        {props.name} is {props.age} years old and is {isOn?"On":"Off"}
+    </div>
+  )
+}
